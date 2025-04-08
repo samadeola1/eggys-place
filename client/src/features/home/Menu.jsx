ced450c75e8a171503dea1c292c9818809a0d300
samadeola1 <oroguns28@gmail.com>
2025-04-08 14:55:55 +0100

    fixes

--- client/src/features/home/Menu.jsx
+++ client/src/features/home/Menu.jsx
@@ -91,7 +91,7 @@ getMenu()
                   return (
                     <div
                       key={_id}
-                      className="card bg-[#252422] w-full md:w-[340px] lg:w-[98%]  p-[16px] my-10 md:my-0 shadow-sm"
+                      className="card bg-[#252422] w-full md:w-[340px] lg:w-[98%]  p-[15px] my-10 md:my-0 shadow-sm"
                     >
                       <Link to={`/product/${_id}`}>
                         <figure>
